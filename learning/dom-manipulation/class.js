61ac593d5eb49c92e8b6c52bb03dacc6513a9dc1
Roger Delgado <rogmide@gmail.com>
2022-06-10 08:47:59 -0400

    append and prepend

--- learning/dom-manipulation/class.js
+++ learning/dom-manipulation/class.js
@@ -1,21 +1,45 @@
-const todos = document.querySelectorAll('li');
+// const todos = document.querySelectorAll('li');
 
 
-function allTodos() {
-    for (const li of todos) {
-        li.classList.toggle('completed');
-    }
-}
+// function allTodos() {
+//     for (const li of todos) {
+//         li.classList.toggle('completed');
+//     }
+// }
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-setInterval(() => {
+// setInterval(() => {
 
-    if (h1.classList.contains('big')) {
-        h1.innerText = "SAD";
-    } else {
-        h1.innerText = "HAPPY";
-    }
-    h1.classList.toggle('big');
-    h1.classList.toggle('small');
-}, 1000);
+//     if (h1.classList.contains('big')) {
+//         h1.innerText = "SAD";
+//     } else {
+//         h1.innerText = "HAPPY";
+//     }
+//     h1.classList.toggle('big');
+//     h1.classList.toggle('small');
+// }, 1000);
+
+const newTodo = document.createElement('li');
+const boldText = document.createElement('b');
+boldText.textContent = "Dont forget to lock";
+const ul = document.querySelector('ul');
+
+newTodo.append(boldText);
+ul.append(newTodo);
+
+newTodo.classList.add('todo');
+
+const secondTodo = document.createElement('li');
+secondTodo.textContent = "Order Some La croix";
+
+ul.append(newTodo, secondTodo);
+
+secondTodo.classList.add('todo');
+
+const newImg = document.createElement('img');
+newImg.setAttribute('src', 'dogss.jpg')
+newImg.classList.add('imgT');
+
+
+document.body.prepend(newImg);
