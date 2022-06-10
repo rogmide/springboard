5e214bbca8b2eb209230000f9412b5d89445c51e
Roger Delgado <rogmide@gmail.com>
2022-06-10 08:31:42 -0400

    classlist toggle learning

--- learning/dom-manipulation/class.js
+++ learning/dom-manipulation/class.js
@@ -7,3 +7,15 @@ function allTodos() {
     }
 }
 
+const h1 = document.querySelector('h1');
+
+setInterval(() => {
+
+    if (h1.classList.contains('big')) {
+        h1.innerText = "SAD";
+    } else {
+        h1.innerText = "HAPPY";
+    }
+    h1.classList.toggle('big');
+    h1.classList.toggle('small');
+}, 1000);
