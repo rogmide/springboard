2da550d430358dc353bd49d87980cc222ddc256b
rogmide <rogmide@gmail.com>
2022-06-20 12:21:02 -0400

    starting testing with jasmine

---
+++ testing-jasmine/taxes.test.js
@@ -0,0 +1,9 @@
+it('should calcula te high tax bracket', function () {
+    expect(calculateTaxes(50000)).toEqual(12500);
+    expect(calculateTaxes(100000)).toEqual(25000);
+});
+
+it('should calcula te low tax bracket', function () {
+    expect(calculateTaxes(10000)).toEqual(1500);
+    expect(calculateTaxes(1000)).toEqual(150);
+});
