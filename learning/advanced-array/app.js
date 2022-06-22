d053f292b6fbfb31c907e4b3d80c664cecc43c3b
rogmide <rogmide@gmail.com>
2022-06-22 10:54:03 -0400

    find and findIndex exercise

--- learning/advanced-array/app.js
+++ learning/advanced-array/app.js
@@ -101,17 +101,17 @@ function every(array, callback){
   return true;
 } */
 
-let numbers3 = [1,2,3];
+let numbers3 = [1, 2, 3];
 
-const num3 = numbers3.every(function(value, index, array){
-  return value > 0;
+const num3 = numbers3.every(function (value, index, array) {
+    return value > 0;
 });
 
 // true
-let numbers4 = [1,2,3];
+let numbers4 = [1, 2, 3];
 
-const num4 = numbers4.every(function(value, index, array){
-  return value > 2;
+const num4 = numbers4.every(function (value, index, array) {
+    return value > 2;
 });
 
 /*
@@ -126,3 +126,96 @@ function hasNoDuplicates(arr) {
         return arr.indexOf(element) !== index;
     });
 }
+
+const scores = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    55,
+    59,
+    69,
+    73,
+    73,
+    75,
+    79,
+    83,
+    88,
+    91,
+    93
+];
+
+scores.find(function (score) {
+    return score > 75;
+});
+//79
+
+scores.find(function (score) {
+    return score !== 0 && score % 2 === 0;
+});
+//88
+
+//If you need to find ALL, use filter:
+const evenScores = scores.filter(function (score) {
+    return score % 2 === 0;
+});
+//[0, 0, 0, 0, 0, 0, 0, 0, 88]
+
+const firstEven = scores.findIndex(function (score) {
+    return score !== 0 && score % 2 === 0;
+});
+//16
+
+scores.findIndex(function (score) {
+    return score > 100;
+});
+
+function partition(arr, pivot) {
+    //Find the index we'll use to pivot around
+    const pivotIdx = arr.findIndex(function (el) {
+        return el > pivot;
+    });
+    //create 2 new arrays, using that pivotIdx we just found
+    const left = arr.slice(0, pivotIdx);
+    const right = arr.slice(pivotIdx);
+    //return both arrays as a single array:
+    return [left, right];
+}
+//partition(scores, 0)
+//Returns...
+// [
+//   [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+//   [ 55, 59, 69, 73, 73, 75, 79, 83, 88, 91, 93 ]
+// ];
+
+// *********************************
+// Writing Our Own Versions
+// *********************************
+function myFind(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr) === true) return arr[i];
+    }
+}
+
+myFind(scores, function (score) {
+    return score > 91;
+});
+
+myFind(scores, function (score) {
+    return score > 100;
+});
+
+function myFindIndex(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr) === true) return i;
+    }
+    return -1;
+}
+
+myFindIndex(scores, function (score) {
+    return score !== 0 && score % 2 == 0;
+});
