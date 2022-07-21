e255a452430361a88b085a6afec9a1794900d817
Roger Delgado <rogmide@gmail.com>
2022-07-21 05:45:01 -0400

    some more exercise done

--- python/exercise/python-ds-practice/30_repeat/repeat.py
+++ python/exercise/python-ds-practice/30_repeat/repeat.py
@@ -18,3 +18,14 @@ def repeat(phrase, num):
         >>> repeat('abc', 'nope') is None
         True
     """
+
+    if not type(num) is int:
+        return True
+
+    if not num >= 0:
+        return True
+
+    temp = ''
+    for i in range(1, num + 1):
+        temp += phrase
+    return temp
