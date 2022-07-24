44625bc0a7bf3a7e1e01ab0f93c9edb9f485e377
Roger Delgado <rogmide@gmail.com>
2022-07-24 08:39:33 -0400

    finishing class exercise

--- python/exercise/python-oo-practice/serial.py
+++ python/exercise/python-oo-practice/serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -18,5 +19,37 @@ class SerialGenerator:
 
     >>> serial.generate()
     100
+
+    Attribute
+    start: int        
+        beginning number of the serial generator        
+    last: int
+        last number generated
+        default value 0 for the start on the obj
+    first: boolean
+        at the start of the class creation we use first
+        to controle when is the first time that the series
+        is created  it will run False until is reset
     """
 
+    def __init__(self, start, last=0, first=True):
+        self.start = start
+        self.last = self.start + last
+        self.first = first
+
+    def __repr__(self):
+        return f'Serial number Start At:  {self.start}, Currenet Serial: {self.last}'
+
+    def generate(self):
+        '''Generate the next serial number in the sequence'''
+        if self.first:
+            self.last = self.last
+            self.first = False
+        else:
+            self.last = self.last + 1
+        return self.last
+
+    def reset(self):
+        '''Reset the number where the sequence start'''
+        self.first = True
+        self.last = self.start
--- python/exercise/python-oo-practice/wordfinder.py
+++ python/exercise/python-oo-practice/wordfinder.py
@@ -1,5 +1,65 @@
-"""Word Finder: finds random words from a dictionary."""
+import os
+from random import randint
 
 
 class WordFinder:
-    ...
+    '''
+    Word Finder: finds random words from a dictionary.
+
+        Attributes
+    path: srt
+        Path will hold the path to the current folder where the app is
+    all_word: list
+        all_word will hold all the letter that appear in the file
+        that is loaded 1 time
+
+    '''
+
+    def __init__(self, file_name, path='', all_word=[]):
+        self.file_name = file_name
+        self.path = path
+        self.all_word = all_word
+        self.path = os.getcwd()
+        self.get_all_word()
+
+    def __repr__(self):
+        return f'Currend word loaded on file: {len(self.all_word)}'
+
+    def get_all_word(self):
+        '''
+        This function fill up the list of all work that are in the file txt
+        '''
+        with open(self.path + f'/{self.file_name}') as file:
+            lines = file.readlines()
+
+        self.all_word = []
+        for i in range(0, len(lines) - 1):
+            self.all_word.append(lines[i][:-1:])
+        self.all_word.append(lines[-1])
+
+    def random(self):
+        '''
+        Return a random work from the list of all_word
+        '''
+        return self.all_word[randint(0, len(self.all_word)-1)]
+
+
+class SpecialWordFinder(WordFinder):
+    '''
+    Special class to handle list of word that have empty space or special char
+    in this case '#'
+    '''
+
+    def __init__(self, file_name, path='', all_word=[]):
+        super().__init__(file_name, path, all_word)
+        self.remove_empty_lines()
+
+    def remove_empty_lines(self):
+        '''
+        Go a check in the list of word if there is any '#' or '' and removed
+        '''
+        for i in range(0, len(self.all_word) - 1):
+            if self.all_word[i].startswith('#'):
+                self.all_word.pop(i)
+        if self.all_word.count('') >= 1:
+            self.all_word.remove('')
