57d78795174d65bd897521b5391ffcffc751472f
Roger Delgado <rogmide@gmail.com>
2022-07-24 10:38:17 -0400

    first step with flask

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,1 +1,45 @@
-print('asdasd')
+# you need to add the next 2 line all the time for using flask
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    html = '''
+    <html>
+        <body>
+            <h1>
+                This is HomePage!
+            </h1>
+            <p>
+                Work Still in Progress!
+            </p>
+        </body>
+    <html>
+    '''
+    return html
+
+
+@app.route('/hello')
+def say_hello():
+    return 'Say Hello'
+
+@app.route('/goodbye')
+def say_bye():
+    return 'Good Bye'
+
+@app.route('/html')
+def real_html():
+    html = '''
+    <html>
+        <body>
+            <h1>
+                Hello!
+            </h1>
+            <p>
+                This is the hello Page!
+            </p>
+        </body>
+    <html>
+    '''
+    return html
