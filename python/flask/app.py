e56147a11db351bedf10cce0fe268143f099d48b
Roger Delgado <rogmide@gmail.com>
2022-07-25 06:13:42 -0400

    learning more python jinja and flask

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,8 +1,10 @@
 # you need to add the next 2 line all the time for using flask
-from crypt import methods
-import re
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from random import randint, choice
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '123'
+debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
@@ -11,7 +13,7 @@ def home_page():
     <html>
         <body>
             <h1>
-                This is HomePage!
+                
             </h1>
             <p>
                 Work Still in Progress!
@@ -24,7 +26,8 @@ def home_page():
 
 @app.route('/hello')
 def say_hello():
-    return 'Say Hello'
+    '''Show Hello Page'''
+    return render_template('hello.html')
 
 
 @app.route('/goodbye')
@@ -80,6 +83,8 @@ def add_comment_form():
         <button>Submit</button>
     </form>
     '''
+
+
 @app.route('/add-comment', methods=['POST'])
 def save_comment():
     comment = request.form['comment']
@@ -97,6 +102,7 @@ def save_comment():
     </ul>
     '''
 
+
 POSTS = {
     1: 'like chicken',
     2: 'mayo is nice',
@@ -104,7 +110,32 @@ POSTS = {
     4: 'Nice Stuff'
 }
 
+
 @app.route('/posts/<int:id>')
 def find_post(id):
     post = POSTS.get(id, 'is not there sorry')
     return f'<p>{post}</p>'
+
+
+#using variables to send to the html template
+#that we can use like this <h1> {{variable here}} </h1> in the html
+@app.route('/lucky')
+def lucky_number():
+    num = randint(1, 3)
+    return render_template('lucky.html', lucky_num=num, msg='Your so Lucky!')
+
+@app.route('/form')
+def show_form():
+    return render_template('form.html')
+
+COMPLIMENTS = ['cool', 'clever', 'tenacious', 'awesome', 'Pythonic']
+
+@app.route('/greet')
+def get_greeting():
+    nice_thing = choice(COMPLIMENTS)
+    name = request.args['username']
+    return render_template('greet.html', username=name, compliment=nice_thing)
+
+@app.route('/spell/<word>')
+def spell_word(word):    
+    return render_template('spell_word.html', word=word)
