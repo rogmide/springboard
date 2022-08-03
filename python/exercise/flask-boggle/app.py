3395e499ec1de3ea244caa659008143624392ff6
Roger Delgado <rogmide@gmail.com>
2022-08-03 06:55:10 -0400

    more changes

--- python/exercise/flask-boggle/app.py
+++ python/exercise/flask-boggle/app.py
@@ -1,5 +1,7 @@
+from crypt import methods
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+import json
 from boggle import Boggle
 boggle_game = Boggle()
 
@@ -14,9 +16,18 @@ def get_home():
     session['board'] = Boggle().make_board()
     return render_template('board.html', board=session['board'])
 
+
 @app.route('/check_word/<word>')
 def check_for_word(word):
 
     is_a_word = Boggle().check_valid_word(session['board'], word)
-    
+
     return {"result": is_a_word}
+
+
+@app.route('/keep_score/', methods=['POST'])
+def kepp_score_and_time_play():
+    req = request.get_json('json_data')
+    print(req['score'])
+    print(req['time'])
+    return {"result": 'something'}
