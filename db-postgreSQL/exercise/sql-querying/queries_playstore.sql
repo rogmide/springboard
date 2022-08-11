6e58b9e7dc5e6f212b2b774ed61bb7fdc0d61b0e
Roger Delgado <rogmide@gmail.com>
2022-08-10 20:05:08 -0400

    working on more exercise

--- db-postgreSQL/exercise/sql-querying/queries_playstore.sql
+++ db-postgreSQL/exercise/sql-querying/queries_playstore.sql
@@ -1,3 +1,8 @@
 -- Comments in SQL Start with dash-dash --
 
-
+--SELECT app_name from analytics where id = 1880;
+--select id, app_name from analytics where last_updated = '2018-08-01';
+--SELECT category, count(app_name) as number_app from analytics group by category
+--select app_name , reviews from analytics order by reviews desc limit 5
+--SELECT app_name, rating from analytics where rating >= 4.8 
+SELECT category, avg(rating) AS avg_rating FROM analytics GROUP BY category ORDER BY avg_rating DESC
