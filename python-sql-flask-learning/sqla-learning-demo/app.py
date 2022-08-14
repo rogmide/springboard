f03275a00f6b33ca2d51f3fafc1faa7fdf588067
Roger Delgado <rogmide@gmail.com>
2022-08-14 16:05:00 -0400

    lerning db, python, flask

--- python-sql-flask-learning/sqla-learning-demo/app.py
+++ python-sql-flask-learning/sqla-learning-demo/app.py
@@ -1,11 +1,10 @@
-from doctest import FAIL_FAST
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
 # Connection to the database that you are going to be working
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
 # This is going to go away in some future time so set it to false so is not annoying
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # basiclly is going to print the Query that we going to be sending to the DB
@@ -19,7 +18,21 @@ def home_page():
     '''Show Home Page'''
 
     # we dont use this but better here than the console that will be lost
-    movies = db.session.execute('SELECT * FROM movies')
-    print(list(movies))  
+    # movies = db.session.execute('SELECT * FROM movies')
+    # print(list(movies))
+
+    # Create the Tables in the Db using the models
+    # db.create_all()
+
+    # stevie = Pet(name='Stevie Chicks', species='chicken', hunger=13)
+    # rocket = Pet(name='rocket', species='mecha-chicken', hunger=1)
+    # toadie = Pet(name='toadie', species='bio-chicken', hunger=12)
+
+    # # add multiple item at the same time using a list
+    # db.session.add_all([stevie, rocket, toadie])
+
+    # # add a signle item at the time
+    # db.session.add(stevie)
+    # db.session.commit()
 
     return render_template('home.html')
--- python-sql-flask-learning/sqla-learning-demo/models.py
+++ python-sql-flask-learning/sqla-learning-demo/models.py
@@ -2,8 +2,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
 # Model go Below
+
+
+class Pet(db.Model):
+    '''Pet Model'''
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    name = db.Column(db.String(50),
+                     nullable=False,
+                     unique=True)
+
+    species = db.Column(db.String(50),
+                        nullable=True)
+
+    hunger = db.Column(db.Integer,
+                       nullable=False,
+                       default=20)
