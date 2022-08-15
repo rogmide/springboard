0b4903e3f6ede06bc24861ed5fe1ecbeb9e6259e
Roger Delgado <rogmide@gmail.com>
2022-08-15 12:44:34 -0400

    lerning more about sqlalchemis

--- python-sql-flask-learning/sqla-learning-demo/app.py
+++ python-sql-flask-learning/sqla-learning-demo/app.py
@@ -1,3 +1,6 @@
+from crypt import methods
+from curses import flash
+from hashlib import new
 from flask import Flask, request, redirect, render_template
 from models import db, connect_db, Pet
 
@@ -13,9 +16,10 @@ app.config['SECRET_KEY'] = "123"
 
 connect_db(app)
 
+
 @app.route('/')
-def home_page():
-    '''Show Home Page'''
+def list_pets():
+    '''Show list of all pet in db'''
 
     # we dont use this but better here than the console that will be lost
     # movies = db.session.execute('SELECT * FROM movies')
@@ -24,15 +28,83 @@ def home_page():
     # Create the Tables in the Db using the models
     # db.create_all()
 
+    # ADDIN ADDIN ADDIN
     # stevie = Pet(name='Stevie Chicks', species='chicken', hunger=13)
     # rocket = Pet(name='rocket', species='mecha-chicken', hunger=1)
-    # toadie = Pet(name='toadie', species='bio-chicken', hunger=12)
+    # toadie = Pet(name='Ahri', species='Leeee', hunger=12)
 
     # # add multiple item at the same time using a list
     # db.session.add_all([stevie, rocket, toadie])
 
     # # add a signle item at the time
-    # db.session.add(stevie)
+    # db.session.add(toadie)
     # db.session.commit()
 
-    return render_template('home.html')
+    # try:
+    #     problem_pet = Pet(name='rocket', species='dog')
+    #     db.session.add(problem_pet)
+    #     db.session.commit()
+    # except:
+    #     # rollback will clean the db.session
+    #     db.session.rollback()
+    #     print("SomeError")
+
+    # UPDATE UPDATE UPDATE
+    # toadie = Pet(name='Ahri', species='Leeee', hunger=12)
+    # toadie.name = 'NewName-Ahri2'
+    # db.session.add(toadie)
+    # db.session.commit()
+    # print(toadie.name)
+
+    # toadie.name = 'anotherAhri'
+    # db.session.add(toadie)
+    # db.session.commit()
+
+    # oadie = Pet(name='oadie', species='Leeee', hunger=12)
+    # db.session.add(oadie)
+    # db.session.commit()
+    # print(oadie)
+    # # <Pet id=27 name=oadie species=Leeee hunger=12>
+
+    # pet_list = Pet.query.all()
+    # get(id) what is inside is the id
+    # pet = Pet.query.get(7)
+    # # filter_by(One or Many Args filter items)
+    # select_where = Pet.query.filter_by(hunger=1).all()
+    # select_where = Pet.query.filter_by(hunger=1).first()
+
+    # DELETE DELETE DELETE
+    # select_where = Pet.query.filter_by(hunger=1).delete()
+    # db.session.add(select_where)
+    # db.session.commit()
+
+    pets = Pet.query.all()
+    return render_template('list.html', pets=pets)
+
+
+@app.route('/<int:pet_id>')
+def show_pet(pet_id):
+    '''Show detail about a single pet'''
+
+    pet = Pet.query.get_or_404(pet_id)
+    return render_template('details.html', pet=pet)
+
+
+@app.route('/', methods=['POST'])
+def create_pets():
+    name = request.form['name']
+    species = request.form['species']
+    hunger = request.form['hunger']
+    hunger = int(hunger) if hunger else None
+
+    new_pet = Pet(name=name, species=species, hunger=hunger)
+    db.session.add(new_pet)
+    db.session.commit()
+
+    return redirect(f'/{new_pet.id}')
+
+
+@app.route('/species/<species_id>')
+def show_pets_by_species(species_id):
+    pets = Pet.get_by_species(species_id)
+    return render_template('species.html', pets=pets, species=species_id)
--- python-sql-flask-learning/sqla-learning-demo/models.py
+++ python-sql-flask-learning/sqla-learning-demo/models.py
@@ -15,6 +15,19 @@ class Pet(db.Model):
 
     __tablename__ = 'pets'
 
+    @classmethod
+    def get_by_species(cls, species):
+        return cls.query.filter_by(species=species).all()
+
+    @classmethod
+    def get_all_hungry(cls):
+        return cls.query.filter(Pet.hunger > 10 ).all()
+
+    # this way is a better representation of the class at the time we print the class
+    def __repr__(self):
+        p = self
+        return f'<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}>'
+
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
@@ -29,3 +42,12 @@ class Pet(db.Model):
     hunger = db.Column(db.Integer,
                        nullable=False,
                        default=20)
+
+    def greet(self):
+        '''Print Data of the Pet'''
+        return f'Hi, i am {self.name} the {self.species}'
+
+    def feed(self, amount=20):
+        '''Update hunger base off of amount'''
+        self.hunger -= amount
+        self.hunger = max(self.hunger, 0)
