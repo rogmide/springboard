235b19845ebf7d5db2c41bf21509fcf0e1211284
Roger Delgado <rogmide@gmail.com>
2022-08-16 22:45:28 -0400

    working in a user creation exercise

--- python/exercise/flask-blogly/app.py
+++ python/exercise/flask-blogly/app.py
@@ -1,18 +1,53 @@
+from crypt import methods
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db
+from models import User, db, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///users_exercise'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "123"
 
 connect_db(app)
+# db.create_all()
 
 
 @app.route('/')
 def home_page():
     '''Show Home Page'''
 
-    return render_template('home.html')
+    return redirect('/users')
+
+
+@app.route('/users')
+def get_users_page():
+    '''Show Users Page'''
+
+    users = User.get_all_users()
+
+    return render_template('users.html', users=users)
+
+
+@app.route('/users/new')
+def get_new_user_from():
+    '''Get New User From Page'''
+
+    return render_template('new_user.html')
+
+
+@app.route('/users/new', methods=['POST'])
+def add_user_to_db():
+    '''Add a new user to the db'''
+
+    print('FINISH WORK HERE FOR TOMORROW')
+    return redirect('/users')
+
+
+@app.route('/users/<int:id>')
+def get_user_detail_page(id):
+    '''Show User Details Page'''
+
+    user = User.get_user_by_id(id)
+
+    return render_template('user_details.html', user=user)
--- python/exercise/flask-blogly/models.py
+++ python/exercise/flask-blogly/models.py
@@ -2,8 +2,47 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
 # Model go Below
+
+
+class User(db.Model):
+    '''Pet Model'''
+
+    __tablename__ = 'users'
+
+    @classmethod
+    def get_all_users(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_user_by_id(cls, id):
+        return cls.query.get(id)
+
+    # this way is a better representation of the class at the time we print the class
+    def __repr__(self):
+        user = self
+        return f'<User id={user.id} name={user.first_name} last_name={user.last_name} image_url=SomeIMg>'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(50),
+                           nullable=False,
+                           unique=True)
+
+    last_name = db.Column(db.String(50),
+                          nullable=False)
+
+    image_url = db.Column(db.Text,
+                          nullable=True,
+                          default='https://cdn-icons-png.flaticon.com/512/906/906331.png?w=826&t=st=1660696447~exp=1660697047~hmac=794234f31e068797117461494bd6b8f0f9889bcb0927355efe4b454f6bd7e9e5')
+
+    def greet(self):
+        '''Print Data of the Pet'''
+        return f'Hi, i am {self.first_name} the {self.last_name}'
