4cef0202f57a7027c88ff7ef3d5d335d1250fd2a
Roger Delgado <rogmide@gmail.com>
2022-08-19 20:28:38 -0400

    lerning about relationship sqlalchemy

--- python-sql-flask-learning/sqla-association-learning/app.py
+++ python-sql-flask-learning/sqla-association-learning/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db
+from models import Department, Employee, db, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///employees_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "123"
@@ -14,5 +14,17 @@ connect_db(app)
 @app.route('/')
 def home_page():
     '''Show Home Page'''
+
+    # db.drop_all()
+    # db.create_all()
+
+    #  Testing
+    # d = Department(dept_code='lgl', dept_name='legal',
+    #                dept_phone='123-4567')
+
+    # e = Employee(name='Bod Bobbins', state='AK', dept_code='lgl')
+
+    # db.session.add(e)
+    # db.session.commit()
 
     return render_template('home.html')
--- python-sql-flask-learning/sqla-association-learning/models.py
+++ python-sql-flask-learning/sqla-association-learning/models.py
@@ -2,8 +2,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
 # Model go Below
+
+
+class Department(db.Model):
+    '''Deparment Model'''
+
+    __tablename__ = "department"
+
+    dept_code = db.Column(db.Text, primary_key=True)
+    dept_name = db.Column(db.Text, nullable=False, unique=True)
+    dept_phone = db.Column(db.Text)
+
+
+class Employee(db.Model):
+    '''Employee Model'''
+
+    __tablename__ = "employee"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    state = db.Column(db.Text, nullable=False, default='CA')
+    dept_code = db.Column(db.Text, db.ForeignKey('department.dept_code'))
