41f78c2bd53c2834c55d0ba17faec8dbfa0565eb
Roger Delgado <rogmide@gmail.com>
2022-08-28 14:36:34 -0400

    more lerning about flask apis

--- python-sql-flask-learning/flask-ext-apis-learning/app.py
+++ python-sql-flask-learning/flask-ext-apis-learning/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, redirect, render_template, flash
+import requests
+from hidden_stuff import API_SECRET_KEY
+from flask import Flask, redirect, render_template, flash, request
 from models import db, connect_db
+
+
+API_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1'
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///library'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = "123"
@@ -11,8 +16,35 @@ app.config['SECRET_KEY'] = "123"
 connect_db(app)
 
 
-@app.route('/')
-def home_page():
-    '''Show Home Page'''
+def get_coords(address):
+    '''Get coords from a address'''
 
-    return render_template('home.html')
+    res = requests.get(f'{API_BASE_URL}/address',
+                       params={'key': API_SECRET_KEY, 'location': address})
+
+    data = res.json()
+
+    lat = data['results'][0]['locations'][0]['latLng']['lat']
+    lng = data['results'][0]['locations'][0]['latLng']['lng']
+
+    coords = {'lat': lat, 'lng': lng}
+
+    return coords
+
+
+@app.route('/')
+def show_address_form():
+    '''Show form to enter address'''
+
+    return render_template('address_form.html')
+
+
+@app.route('/geocode')
+def get_location():
+    '''Get addres form the API'''
+
+    address = request.args['address']
+
+    coords = get_coords(address)
+
+    return render_template('address_form.html', coords=coords)
--- python-sql-flask-learning/flask-ext-apis-learning/mapquest.py
+++ python-sql-flask-learning/flask-ext-apis-learning/mapquest.py
@@ -1,5 +1,5 @@
 import requests
-from secrets import API_SECRET_KEY
+from hidden_stuff import API_SECRET_KEY
 
 BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address'
 location = '11211 Causeway Blvd, Brandon, FL 33511'
