7f0bbde0ecff6dc11c288d10066143f9de41d2bc
Roger Delgado <rogmide@gmail.com>
2022-08-29 20:04:35 -0400

    finishing class

--- python-sql-flask-learning/flask-ext-apis-learning/static/app.js
+++ python-sql-flask-learning/flask-ext-apis-learning/static/app.js
@@ -1,4 +1,10 @@
-$(".delete_todo").on("click", function (e) {
-    const id = $(this).data('id')
-    alert(id)
-});
+$(".delete_todo").on("click", deleteTodo);
+
+async function deleteTodo() {
+  /*Delet a todo from the DB*/
+
+  const id = $(this).data("id");
+  res = await axios.delete(`/api/todos/${id}`);
+  alert("Deleted!!!");
+  $(this).parent().remove();
+}
