d2e083bc365748b649d9fde8fbebdb78cb26091d
Roger Delgado <rogmide@gmail.com>
2022-09-16 19:29:49 -0400

    exercise with FS done

--- node-js/exercises/asynchronous/app.js
+++ node-js/exercises/asynchronous/app.js
@@ -1,4 +1,14 @@
 // #########################################################################
+// DOM Manipulation Variables
+// #########################################################################
+
+$btn_catch_pokemon = $("#btn_catch_pokemon");
+$numbers = $("#numbers");
+$btn_gimme_card = $("#btn_gimme_card");
+$card_holder = $("#card_holder");
+$pokemon_holder = $("#pokemon_holder");
+
+// #########################################################################
 // Random Numbers to Show facts
 // #########################################################################
 promNumbers = [];
@@ -7,9 +17,6 @@ for (let i = 0; i < 4; i++) {
     axios.get(`http://numbersapi.com/${Math.round(Math.random() * 100)}`)
   );
 }
-$numbers = $("#numbers");
-$btn_gimme_card = $("#btn_gimme_card");
-$card_holder = $("#card_holder");
 
 promNumbers.forEach((resp) => {
   resp.then((res) => $numbers.append(`<li>${res.data}</li>`));
@@ -58,3 +65,50 @@ $btn_gimme_card.on("click", function () {
 // #########################################################################
 // End of Deck of Cards Draw
 // #########################################################################
+
+// #########################################################################
+// Star of Pokemon Request to API
+// #########################################################################
+
+let poke_name = "";
+let poke_img = "";
+let description = "";
+
+$btn_catch_pokemon.on("click", function (e) {
+
+    card = axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${Math.round(Math.random() * 100)}/`
+    );
+
+    card
+      .then((resp) => {
+        // console.log(resp);
+        poke_name = resp.data.name;
+        poke_img = resp.data.sprites.front_default;
+        return axios.get(resp.data.species.url);
+      })
+
+      // const found_names = names.filter(v => v.name === "Joe" && v.age < 30);
+      .then((resp) => {
+        eng_description = resp.data.flavor_text_entries.filter(
+          (t) => t.language.name === "en"
+        );
+        description =
+          eng_description[Math.round(Math.random() * eng_description.length)]
+            .flavor_text;
+        // console.log(description);
+      })
+      .catch((err) => console.log(err));
+
+    $pokemon_holder.children().length >= 3
+      ? $pokemon_holder.empty()
+      : $pokemon_holder.append(`
+    <div class="card" style="width: 18rem;">
+      <img id="poke_img" src="${poke_img}" class="card-img-top" alt="Pokemon Img">
+      <div class="card-body">
+      <h5 class="card-title">${poke_name}</h5>
+      <p class="card-text">${description}</p>
+      </div>
+    </div>`);
+  
+});
