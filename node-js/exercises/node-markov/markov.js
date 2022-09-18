7366deeb052e75f29f74cd77ed73663dd3b5bec8
Roger Delgado <rogmide@gmail.com>
2022-09-18 12:04:33 -0400

    adding jquery to project

--- node-js/exercises/node-markov/markov.js
+++ node-js/exercises/node-markov/markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -18,8 +16,18 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-  }
+    let res = {};
 
+    for (let i = 0; i < this.words.length; i++) {
+      if (!(this.words[i] in res)) {
+        res[this.words[i]] = [this.words[i + 1]];
+      } else {
+        res[this.words[i]].push(this.words[i + 1] ? this.words[i + 1] : null);
+      }
+    }
+    console.log(res);
+    return res;
+  }
 
   /** return random text from chains */
 
@@ -27,3 +35,9 @@ class MarkovMachine {
     // TODO
   }
 }
+
+const mm = new MarkovMachine("the cat in the hat is in the hat");
+const mm2 = new MarkovMachine("in the hat is in the hat");
+// const mm3 = new MarkovMachine(
+//   "You are old,’ said the Dormouse, who was talking. Alice could only see her. She is such a new pair of white kid gloves and the blades of grass, but she remembered the number of bathing machines in the kitchen that did not like the wind, and was just beginning to grow up any more if you’d like it put the Dormouse again, so she went nearer to make out that it was certainly English. ‘I don’t quite understand you,’ she said, ‘for her hair goes in such confusion that she was looking down with it."
+// );
