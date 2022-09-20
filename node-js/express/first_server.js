356bc2f44f7220c361ad91434867a316c2f3506e
Roger Delgado <rogmide@gmail.com>
2022-09-20 18:23:26 -0400

    learning express

--- node-js/express/first_server.js
+++ node-js/express/first_server.js
@@ -2,6 +2,30 @@ const express = require("express");
 
 const app = express();
 
+// ########################################################
+// ROUTES DECLARATION LIKE LIKE FLASK
+app.get("/", (req, res) => {
+  console.log("Dogs do Something");
+  return res.send([1, 2, 3]);
+});
+
+app.get("/dogs", (req, res) => {
+  console.log("Dogs do Something");
+  return res.send("<h1>SOmething</h1>");
+});
+
+// ########################################################
+// SAME ROUTE PATH - BUT IS APP.GET - APP.POST APP.PATCH ETC...
+app.get("/chickens", (req, res) => {
+  res.send("BOCK! BOCK! BOCK! - GET REQUEST");
+});
+
+app.post("/chickens", (req, res) => {
+  res.send("you create a new chickent - POST REQUEST");
+});
+
+// ########################################################
+// app.listen should be always at end of the file
 app.listen(3000, () => {
   console.log("App running on  http://127.0.0.1:3000/");
 });
