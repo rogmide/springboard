3975e34074e602bd36a22ffc21ce68e26dbb6790
Roger Delgado <rogmide@gmail.com>
2022-09-24 14:45:50 -0400

    middleware learning

--- node-js/express-router-middleware/app.js
+++ node-js/express-router-middleware/app.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const ExpressError = require("./expressError");
 const userRoute = require("./userRoutes");
+const middleware = require("./middleware");
+const morgan = require("morgan");
 
 const app = express();
 
-function logger(req, res, next) {
-  console.log(`Receiveda ${req.method} request to ${req.path}`);
-  return next();
-}
+//########################################################
+//This is middleware run after the request and before the response
+//middleware has to be on the top if you want it to run before everything
+// app.use(middleware.logger);
 
-app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// ########################################################
+// External middleware
+app.use(morgan("dev"));
 
 // ########################################################
 // This will pre-fix route sample: /users /userRoute that are there
 app.use("/users", userRoute);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// ########################################################
+// middleware pas as a sec argument to the app.use to run before the route
+app.use("/secret", middleware.checkPassword, (req, res, next) => {
+  return res.send("I love you FOR Real Marry Me");
+});
+
+app.use("/private", middleware.checkPassword, (req, res) => {
+  return res.send("THIS SECTION IS PRIVATE - BUT NOT TO YOU");
+});
 
 // ########################################################
 // ERROR HANDELER 404
@@ -31,8 +44,4 @@ app.use((err, req, res, next) => {
   return res.status(status).json({ error: { message, status } });
 });
 
-// ########################################################
-// app.listen should be always at end of the file
-app.listen(3000, () => {
-  console.log("App running on  http://127.0.0.1:3000/");
-});
+module.exports = app;
