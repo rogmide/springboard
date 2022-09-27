b78428d705aab6fd17e6e1f0fbfc7264b0e93099
Roger Delgado <rogmide@gmail.com>
2022-09-27 10:22:45 -0400

    settin up learning env for pg

---
+++ node-js/learning/express-pg-learning/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const ExpressError = require("./expressError");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+// ########################################################
+// ERROR HANDELER 404
+app.use((req, res, next) => {
+  const e = new ExpressError("Page Not Found", 404);
+  next(e);
+});
+
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  let message = err.msg || "Default Msg";
+  return res.status(status).json({ error: { message, status } });
+});
+
+module.exports = app;
