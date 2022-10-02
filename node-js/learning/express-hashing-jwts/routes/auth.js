4b55e6dcba1f67d6bdbf0f0a42d0bbc5fb42cfb2
Roger Delgado <rogmide@gmail.com>
2022-10-02 11:11:29 -0400

    adding jwt

--- node-js/learning/express-hashing-jwts/routes/auth.js
+++ node-js/learning/express-hashing-jwts/routes/auth.js
@@ -30,6 +30,39 @@ router.post("/register", async (req, res, next) => {
     );
     return res.json(results.rows[0]);
   } catch (error) {
+    // ######################################
+    // 23505 code for duplicate key in the db
+    if (error.code === "23505")
+      return next(
+        new ExpressError(`Username is already taken, Please pick another!`, 400)
+      );
+    return next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password)
+      throw new ExpressError(`Username and password require`, 400);
+
+    const results = await db.query(
+      `
+    select username, password
+    from users
+    where username = $1
+    `,
+      [username]
+    );
+
+    const user = results.rows[0];
+    if (user) {
+      if (await bcrypt.compare(password, user.password)) {
+        return res.json({ msg: `Logged in!` });
+      }
+    }
+    throw new ExpressError(`Invalid username/password!`, 404);
+  } catch (error) {
     return next(error);
   }
 });
