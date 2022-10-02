25ed82703b227b41b7465ad38c587db22610ce09
Roger Delgado <rogmide@gmail.com>
2022-10-02 10:06:32 -0400

    adding password hashing

--- node-js/learning/express-hashing-jwts/db.js
+++ node-js/learning/express-hashing-jwts/db.js
@@ -1,19 +1,13 @@
-/** Database setup for BizTime. */
-
+/** Database for auth-api demo. */
 const { Client } = require("pg");
+const { DB_URI } = require("./config");
 
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///express_auth_test";
-} else {
-  DB_URI = "postgresql:///express_auth";
-}
-
-let db = new Client({
-  connectionString: DB_URI,
+const client = new Client({
+  connectionString: DB_URI
 });
 
-db.connect();
+client.connect();
 
-module.exports = db;
+
+module.exports = client;
+
--- node-js/learning/express-hashing-jwts/routes/auth.js
+++ node-js/learning/express-hashing-jwts/routes/auth.js
@@ -2,14 +2,35 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
 
-// ############################################
-// GET /companies Get All Companies
 router.get("/", async (req, res, next) => {
   try {
     res.send("API IS WORKING");
   } catch (error) {
     next(error);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password)
+      throw new ExpressError(`Username and password require`, 400);
+
+    const hashpwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const results = await db.query(
+      `
+    insert into users (username, password)
+    values ($1, $2)
+    returning username
+    `,
+      [username, hashpwd]
+    );
+    return res.json(results.rows[0]);
+  } catch (error) {
+    return next(error);
   }
 });
 
