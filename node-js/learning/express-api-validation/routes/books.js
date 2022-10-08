a3d6debe45f462da93f53e4ef06d82450f9965f9
Roger Delgado <rogmide@gmail.com>
2022-10-08 11:47:39 -0400

    vakidate schema

--- node-js/learning/express-api-validation/routes/books.js
+++ node-js/learning/express-api-validation/routes/books.js
@@ -4,32 +4,31 @@ const ExpressError = require("../expressError");
 const jsonschema = require("jsonschema");
 const bookSchema = require("../schemas/bookSchema");
 
-router.post("/", function (req, res, next) {
-  const bookData = req.body.book;
+// router.post("/", function (req, res, next) {
+//   const bookData = req.body.book;
 
-  if (!bookData) {
-    let error = new ExpressError("Book data is required", 400);
-    return next(error);
+//   if (!bookData) {
+//     let error = new ExpressError("Book data is required", 400);
+//     return next(error);
+//   }
+
+//   return res.json(bookData);
+// });
+
+router.post("/", function (req, res, next) {
+  // Validate req.body against our book schema:
+  const result = jsonschema.validate(req.body, bookSchema);
+
+  // If it's not valid...
+  if (!result.valid) {
+    //Collect all the errors in an array
+    const listOfErrors = result.errors.map((e) => e.stack);
+    const err = new ExpressError(listOfErrors, 400);
+    return next(err);
   }
 
-  return res.json(bookData);
+  // Do somehting is valid
+  return res.json("THAT IS VALID!");
 });
-
-// router.post("/", function (req, res, next) {
-//   // Validate req.body against our book schema:
-//   const result = jsonschema.validate(req.body, bookSchema);
-
-//   // If it's not valid...
-//   if (!result.valid) {
-//     //Collect all the errors in an array
-//     const listOfErrors = result.errors.map(e => e.stack);
-//     const err = new ExpressError(listOfErrors, 400);
-//     //Call next with error
-//     return next(err);
-//   }
-//   // This is where you would insert something into your DB
-//   // If we make it here, we know the data is validated!
-//   return res.json("THAT IS VALID!");
-// });
 
 module.exports = router;
