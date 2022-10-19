48699d57f904ac8a84912c840d41e871af981e7b
Roger Delgado <rogmide@gmail.com>
2022-10-18 20:21:27 -0400

    more learning reactJS

---
+++ ReactJS/learning/react-intro/movieList.js
@@ -0,0 +1,11 @@
+const MovieList = () => {
+  return (
+    <ul>
+      <li>Amadeus</li>
+      <li>Alien</li>
+      <li>Arrival</li>
+    </ul>
+  );
+};
+
+ReactDOM.render(<MovieList />, document.getElementById("root"));
