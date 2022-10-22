4c18e2b0b05db9dd686e7161356e793420cce38e
Roger Delgado <rogmide@gmail.com>
2022-10-22 11:32:59 -0400

    export/import on react app

--- ReactJS/learning/react-cra/my-app/src/App.js
+++ ReactJS/learning/react-cra/my-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { add, multiply } from "./helpers.js";
+import data, { meow } from "./cats";
 
 function App() {
+  console.log(add(4, 9));
+  console.log(multiply(4, 9));
+  console.log(data);
+  console.log(meow())
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
