445206f9e37b75dfe935829c320b12ec3ea001a9
Roger Delgado <rogmide@gmail.com>
2022-10-23 12:17:17 -0400

    adding commment to methods

--- ReactJS/exercise/pokedex/src/helpers.js
+++ ReactJS/exercise/pokedex/src/helpers.js
@@ -1,15 +1,20 @@
+// Return a Array with 2 Array - Dex1 and Dex2 holding random pokemons
 const randPokemon = (poke_data) => {
+  // Scramble Array
   poke_data.sort(() => 0.5 - Math.random());
+
+  // Make 2 dexs from the Scramble Array poke_data
   const dex1 = poke_data.slice(4, poke_data.lenght);
   const dex2 = poke_data.slice(0, 4);
   return [dex1, dex2];
 };
 
+// Calculta Total EXP
 const totalExp = (poke_data) => {
   const total = poke_data.reduce((acc, p) => {
     return acc + p.base_experience;
   }, 0);
-  return total
+  return total;
 };
 
 // eslint-disable-next-line
