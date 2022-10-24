288ef35cc0998d9b00a2bba5d6c72f788b76d2e9
Roger Delgado <rogmide@gmail.com>
2022-10-24 14:08:19 -0400

    more state learning

--- ReactJS/learning/react-events-state/src/NumberGame.js
+++ ReactJS/learning/react-events-state/src/NumberGame.js
@@ -8,6 +8,7 @@ const NumberGame = (props) => {
   // guess and target variable are unchange until a event happen
   const [guess, setGuess] = useState(genRandom());
   const [target, setTarget] = useState(genRandom());
+  const [count, setCount] = useState(0);
   const isWinner = target === guess;
   return (
     <div className="NumberGame">
@@ -15,7 +16,27 @@ const NumberGame = (props) => {
       <h1 className={isWinner ? "winner" : "loser"}>Your Guess: {guess}</h1>
       {/* Nice way to hide something */}
       {!isWinner && (
-        <button onClick={() => setGuess(genRandom())}>Generate Num</button>
+        <button
+          onClick={() => {
+            setGuess(genRandom());
+            setCount(count + 1);
+          }}
+        >
+          Generate Num
+        </button>
+      )}
+      <h1>Guess Times: {count}</h1>
+
+      {isWinner && (
+        <button
+          onClick={() => {
+            setTarget(genRandom());
+            setGuess(0);
+            setCount(0);
+          }}
+        >
+          New Game
+        </button>
       )}
     </div>
   );
