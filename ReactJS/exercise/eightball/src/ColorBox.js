ddcde189abe45814f97a5cf81c5ec203edee64ab
Roger Delgado <rogmide@gmail.com>
2022-10-25 11:21:43 -0400

    setting up FS 2

---
+++ ReactJS/exercise/eightball/src/ColorBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ColorBox = () => {
+  return (
+    <div>
+      <div>!!!BOX HERE!!!</div>
+      <button>Change</button>
+    </div>
+  );
+};
+
+export default ColorBox;
