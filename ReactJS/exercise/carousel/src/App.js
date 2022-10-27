c677c948c8d9fe92adf2d42c3f9f06899ee96f97
Roger Delgado <rogmide@gmail.com>
2022-10-27 11:13:18 -0400

    fixing app to start properlly

--- ReactJS/exercise/carousel/src/App.js
+++ ReactJS/exercise/carousel/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Carousel from "./Carousel";
-import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
       <Carousel />
-      <Card />
     </div>
   );
 }
--- ReactJS/exercise/react-state-carousel-coins/carousel/src/Card.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import Card from "./Card";
-
-// Smoke Test
-test("should render without crashing", () => {
-  render(<Card />);
-});
