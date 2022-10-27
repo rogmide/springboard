4abf885831fdfde01737a615847d94128edba39b
Roger Delgado <rogmide@gmail.com>
2022-10-27 17:55:57 -0400

    1 test split into 3 different test

--- ReactJS/exercise/coinflip/src/CoinGame.test.js
+++ ReactJS/exercise/coinflip/src/CoinGame.test.js
@@ -24,13 +24,44 @@ test("should match snapshot", () => {
 it("works when the img is not there", function () {
   const { queryByTestId } = render(<CoinGame />);
   const coin_img = queryByTestId("coin_img");
-  const flip_btn = queryByTestId("flip_btn");
   expect(coin_img).toBeNull();
 
+  const flip_btn = queryByTestId("flip_btn");
   fireEvent.click(flip_btn);
   const coin_img_there = queryByTestId("coin_img");
   expect(coin_img_there).toHaveAttribute("src", "coin-Front.png");
 
   fireEvent.click(flip_btn);
+  expect(coin_img_there).toHaveAttribute("src", "coin-Back.png");
+});
+
+it("works when the img is head/front (after one click)", function () {
+  const { queryByTestId } = render(<CoinGame />);
+
+  const flip_btn = queryByTestId("flip_btn");
+  fireEvent.click(flip_btn);
+
+  // not Null
+  const coin_img = queryByTestId("coin_img");
+  expect(coin_img).not.toBeNull();
+
+  // Checking the src = coin-Front.png
+  const coin_img_there = queryByTestId("coin_img");
+  expect(coin_img_there).toHaveAttribute("src", "coin-Front.png");
+});
+
+it("works when the img is tail/back (after two click)", function () {
+  const { queryByTestId } = render(<CoinGame />);
+
+  const flip_btn = queryByTestId("flip_btn");
+  fireEvent.click(flip_btn);
+  fireEvent.click(flip_btn);
+
+  // not Null
+  const coin_img = queryByTestId("coin_img");
+  expect(coin_img).not.toBeNull();
+
+  // Checking the src = coin-Front.png
+  const coin_img_there = queryByTestId("coin_img");
   expect(coin_img_there).toHaveAttribute("src", "coin-Back.png");
 });
