6e125642db598ead84c0b5c86cd35201dbf774a1
Roger Delgado <rogmide@gmail.com>
2022-11-03 09:37:59 -0400

    more about useEffect

--- ReactJS/learning/use-effects/src/App.js
+++ ReactJS/learning/use-effects/src/App.js
@@ -1,14 +1,17 @@
 // import logo from './logo.svg';
 import "./App.css";
-import Timer from "./Timer";
+// import Timer from "./Timer";
 // import Counter from "./Counter";
+import ProfileViwer from "./ProfileViwer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <Counter /> */}
-        <Timer />
+        {/* <Timer /> */}
+        <ProfileViwer name="rogmide" />
+        <ProfileViwer name="colt" />
       </header>
     </div>
   );
---
+++ ReactJS/learning/use-effects/src/ProfileViwer.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// 'https://api.github.com/users/rogmide'
+
+const ProfileViwer = ({ name }) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function loadProfile() {
+      const res = await axios.get(`https://api.github.com/users/${name}`);
+      setData(res.data.name);
+    }
+    loadProfile();
+    // If you using a useState on the useEffect you should
+    // added as a dependency on the useEffect
+    // that way if the useState change
+    // the useEffect is run again
+  }, [name]);
+
+  return (
+    <div>
+      <h3>{data ? data : "Loading..."}</h3>
+    </div>
+  );
+};
+
+export default ProfileViwer;
