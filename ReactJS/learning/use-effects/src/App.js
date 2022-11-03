5b7ed4ebce305809d63695ed9d15a3913a37e856
Roger Delgado <rogmide@gmail.com>
2022-11-03 09:16:15 -0400

    learning more about useEffect

--- ReactJS/learning/use-effects/src/App.js
+++ ReactJS/learning/use-effects/src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 import "./App.css";
-// import Time from "./Timer";
-import Counter from "./Counter";
+import Timer from "./Timer";
+// import Counter from "./Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
+        {/* <Counter /> */}
+        <Timer />
       </header>
     </div>
   );
--- ReactJS/learning/use-effects/src/Timer.js
+++ ReactJS/learning/use-effects/src/Timer.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Time = () => {
   const [sec, setSec] = useState(0);
-  //   setInterval(() => {
-  //     console.log(`Sec: ${sec}`);
-  //     setSec((sec) => sec + 1);
-  //   }, 1000);
+
+  // If we pass a Empty ARRAY is telling
+  // that there is no other dependency
+  // and run only one time
+  useEffect(() => {
+    setInterval(() => {
+      setSec((sec) => sec + 0.5);
+    }, 1000);
+    // passing a Empty Array
+    // run this callback after first render
+    // and dont run it anymore
+    // it will run if the dependency changes
+    // dependency are inside the array
+  }, []);
+
   return (
     <div>
       <h3>{sec}</h3>
