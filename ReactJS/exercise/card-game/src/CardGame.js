92e53c9f92e8365db18ec3e31924d5205e0acb2f
Roger Delgado <rogmide@gmail.com>
2022-11-04 11:19:03 -0400

    exercise working as requested

--- ReactJS/exercise/card-game/src/CardGame.js
+++ ReactJS/exercise/card-game/src/CardGame.js
@@ -5,33 +5,46 @@ import "./CardGame.css";
 import { v4 as uuidv4 } from "uuid";
 
 const CardGame = () => {
-  const INITIAL_STATE = [
-    // default box to work with app
-    {
-      id: uuidv4(),
-      src: "https://images.unsplash.com/photo-1588699219474-fce022b9f633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80",
-    },
-  ];
+  const INITIAL_STATE = [];
 
+  const deckId = useRef();
   const [currDeck, setCurrDeck] = useState(INITIAL_STATE);
   const [deckUrl, setDeckUrl] = useState(
-    "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
+    `https://deckofcardsapi.com/api/deck/new/draw/?count=1`
   );
 
-  //   useEffect(() => {
-  //     async function loadDeck() {
-  //       const res = await axios.get(deckUrl);
-  //       setCurrDeck(res.data.cards[0].image);
-  //     }
-  //     loadDeck();
-  //     // Clean function is basaclly returning a function
-  //     // on the useEffect Scoope
-  //     // return () => console.log("Cleanning Up");
-  //   }, [deckUrl]);
+  const gimeCard = () => {
+    async function getCard() {
+      const res = await axios.get(deckUrl);
+
+      if (res.data.remaining != 0) {
+        setCurrDeck((cDeck) => [
+          ...cDeck,
+          { src: res.data.cards[0].image, id: uuidv4() },
+        ]);
+        deckId.current = res.data.deck_id;
+        setDeckUrl(
+          `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`
+        );
+      } else {
+        setCurrDeck(INITIAL_STATE);
+        deckId.current = "new";
+        setDeckUrl(
+          `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`
+        );
+        alert("New Deck is About to Start!!!");
+      }
+    }
+    getCard();
+  };
 
   return (
     <div id="card_container" className="container">
-      <button id="btn_gimme_card" className="btn btn-success">
+      <button
+        onClick={gimeCard}
+        id="btn_gimme_card"
+        className="btn btn-success"
+      >
         GIMME A CARD
       </button>
 
