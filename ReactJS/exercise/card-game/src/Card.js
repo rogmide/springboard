3ddb816f8fa1a1523e56fe1cf42f6ac3734dae77
Roger Delgado <rogmide@gmail.com>
2022-11-04 11:00:34 -0400

    adding card on the start of app

--- ReactJS/exercise/card-game/src/Card.js
+++ ReactJS/exercise/card-game/src/Card.js
@@ -1,10 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./Card.css";
 
-const Card = () => {
-  const src =
-    "https://images.unsplash.com/photo-1618304925090-b68a8c744cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80";
-
+const Card = ({ src }) => {
   return (
     <div id="card_holder" className="card_holder">
       <img
--- ReactJS/exercise/card-game/src/CardGame.js
+++ ReactJS/exercise/card-game/src/CardGame.js
@@ -1,14 +1,45 @@
 import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
+import axios from "axios";
 import "./CardGame.css";
+import { v4 as uuidv4 } from "uuid";
 
 const CardGame = () => {
+  const INITIAL_STATE = [
+    // default box to work with app
+    {
+      id: uuidv4(),
+      src: "https://images.unsplash.com/photo-1588699219474-fce022b9f633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80",
+    },
+  ];
+
+  const [currDeck, setCurrDeck] = useState(INITIAL_STATE);
+  const [deckUrl, setDeckUrl] = useState(
+    "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
+  );
+
+  //   useEffect(() => {
+  //     async function loadDeck() {
+  //       const res = await axios.get(deckUrl);
+  //       setCurrDeck(res.data.cards[0].image);
+  //     }
+  //     loadDeck();
+  //     // Clean function is basaclly returning a function
+  //     // on the useEffect Scoope
+  //     // return () => console.log("Cleanning Up");
+  //   }, [deckUrl]);
+
   return (
     <div id="card_container" className="container">
       <button id="btn_gimme_card" className="btn btn-success">
         GIMME A CARD
       </button>
-      <Card />
+
+      {currDeck.map((cd) => (
+        <div key={cd.id}>
+          <Card src={cd.src} />
+        </div>
+      ))}
     </div>
   );
 };
