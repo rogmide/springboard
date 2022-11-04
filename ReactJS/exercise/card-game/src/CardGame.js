aaf0cbc6a14af0451dcc1a0f043e40e70f02e077
Roger Delgado <rogmide@gmail.com>
2022-11-04 12:05:12 -0400

    getShuffleDeck added to app

--- ReactJS/exercise/card-game/src/CardGame.js
+++ ReactJS/exercise/card-game/src/CardGame.js
@@ -6,7 +6,6 @@ import { v4 as uuidv4 } from "uuid";
 
 const CardGame = () => {
   const INITIAL_STATE = [];
-
   const deckId = useRef();
   const [currDeck, setCurrDeck] = useState(INITIAL_STATE);
   const [deckUrl, setDeckUrl] = useState(
@@ -26,16 +25,29 @@ const CardGame = () => {
         setDeckUrl(
           `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`
         );
+        console.log(deckId.current);
+        console.log(res.data.remaining);
       } else {
         setCurrDeck(INITIAL_STATE);
         deckId.current = "new";
         setDeckUrl(
           `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`
         );
-        alert("New Deck is About to Start!!!");
+        alert("Error: no cards remaining!");
       }
     }
     getCard();
+  };
+
+  const getShuffleDeck = () => {
+    async function getDeck() {
+      const res = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${deckId.current}/shuffle/?remaining=true`
+      );
+      alert(`Only ${res.data.remaining - 1} card left on the deck!`);
+      setCurrDeck([]);
+    }
+    getDeck();
   };
 
   return (
@@ -46,6 +58,14 @@ const CardGame = () => {
         className="btn btn-success"
       >
         GIMME A CARD
+      </button>
+
+      <button
+        onClick={getShuffleDeck}
+        id="btn_gimme_card"
+        className="btn btn-success"
+      >
+        Shuffle A DECK
       </button>
 
       {currDeck.map((cd) => (
