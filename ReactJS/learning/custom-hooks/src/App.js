f67e6144191d97a6a90ead581dbc542b07f43ff9
Roger Delgado <rogmide@gmail.com>
2022-11-05 09:00:28 -0400

    more learning

--- ReactJS/learning/custom-hooks/src/App.js
+++ ReactJS/learning/custom-hooks/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MoodClicker from "./MoodClicker";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Counter />
         <MoodClicker />
       </header>
     </div>
