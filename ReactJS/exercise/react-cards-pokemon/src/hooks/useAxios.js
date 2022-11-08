e95bd304163c9f117c421d5080bc5c9f564f9d56
Roger Delgado <rogmide@gmail.com>
2022-11-08 09:33:01 -0500

    passing url stray to the method

--- ReactJS/exercise/react-cards-pokemon/src/hooks/useAxios.js
+++ ReactJS/exercise/react-cards-pokemon/src/hooks/useAxios.js
@@ -1,8 +1,8 @@
 import { v4 as uuid } from "uuid";
 import axios from "axios";
 
-const useAxios = (url) => {
-  const getNewData = async (currentUrl = url) => {
+const useAxios = () => {
+  const getNewData = async (currentUrl) => {
     try {
       const res = await axios.get(currentUrl);
       return (oldData) => [...oldData, { ...res.data, id: uuid() }];
