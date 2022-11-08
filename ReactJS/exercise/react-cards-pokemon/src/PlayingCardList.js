6ed1b47bd1a80e909909ee0dc7c9c536da2225e8
Roger Delgado <rogmide@gmail.com>
2022-11-08 14:26:27 -0500

    removing unused variables

--- ReactJS/exercise/react-cards-pokemon/src/PlayingCardList.js
+++ ReactJS/exercise/react-cards-pokemon/src/PlayingCardList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 import useAxios from "./hooks/useAxios";
