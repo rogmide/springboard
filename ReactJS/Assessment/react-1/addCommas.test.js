30e85a9fa627e8902c13e3cf66925a645bf5a73e
Roger Delgado <rogmide@gmail.com>
2022-11-10 12:32:10 -0500

    adding testing to the method addCommas

--- ReactJS/Assessment/react-1/addCommas.test.js
+++ ReactJS/Assessment/react-1/addCommas.test.js
@@ -4,4 +4,16 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("Test simple Num: 1234", () => {
+    expect(addCommas("1234")).toEqual("1,234");
+  });
+
+  test("Test negative Num: -5678", () => {
+    expect(addCommas("-5678")).toEqual("-5,678");
+  });
+
+  test("Test dot Num: -3141592.65", () => {
+    expect(addCommas("-3141592.65")).toEqual("-3,141,592.65");
+  });
 });
