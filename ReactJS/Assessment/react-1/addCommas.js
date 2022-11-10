097f58b9c7567a7799d92457a63c41cf3ac3f15d
Roger Delgado <rogmide@gmail.com>
2022-11-10 12:12:30 -0500

    adding bonus to the method

--- ReactJS/Assessment/react-1/addCommas.js
+++ ReactJS/Assessment/react-1/addCommas.js
@@ -20,28 +20,34 @@ let num3 = "9876543210";
 let num4 = "6";
 let num5 = "-10";
 let num6 = "-5678";
+let num7 = "12345.678";
+let num8 = "-3141592.65";
 
 // Making an Array of 3 items of length and returning it backward
 // Sample
 // input: "9876543210"
-// outpust: [ [ '0', '1', '2' ], [ '3', '4', '5' ], [ '6', '7', '8' ], [ '9' ] ]
+// outpust: Array[0] => [ [ '0', '1', '2' ], [ '3', '4', '5' ], [ '6', '7', '8' ], [ '9' ] ]
+//          Array[1] => Hold the number after the dot
 const threeNumSplit = (stringNum) => {
-  let tempArr = stringNum.split("").reverse();
+  let afterDot = stringNum.split(".");
+  let tempArr = afterDot[0].split("").reverse();
   let result = [];
   while (tempArr.length > 0) {
     result.push(tempArr.splice(0, 3));
   }
-  return result;
+  return [result, afterDot[1]];
 };
 
 function addCommas(num) {
   let result = "";
   let tempArr = threeNumSplit(num);
 
-  result = tempArr
+  let halfNum = tempArr[0]
     .reverse()
     .map((num) => num.reverse().join(""))
     .toString();
+
+  result = halfNum + (tempArr[1] ? `.${tempArr[1]}` : "");
   console.log(result);
   return result;
 }
@@ -52,5 +58,7 @@ addCommas(num3);
 addCommas(num4);
 addCommas(num5);
 addCommas(num6);
+addCommas(num7);
+addCommas(num8);
 
 module.exports = addCommas;
