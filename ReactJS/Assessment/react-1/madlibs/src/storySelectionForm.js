9b1b954cff800a76cf7d00ec3bf6e330890b4aeb
Roger Delgado <rogmide@gmail.com>
2022-11-11 16:25:08 -0500

    adding some css to app

--- ReactJS/Assessment/react-1/madlibs/src/storySelectionForm.js
+++ ReactJS/Assessment/react-1/madlibs/src/storySelectionForm.js
@@ -2,27 +2,14 @@ import React, { useState } from "react";
 import madlibsDB from "./data/madlibsDB";
 import StorysSelections from "./storysSelections";
 
-const StorySelectionForm = ({ renderStory = true }) => {
-  const [formData, setFormData] = useState("");
-
-  const handleChange = (event) => {
-    setFormData(event.target.value);
-  };
+const StorySelectionForm = () => {
+  const handleChange = (event) => {};
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
-  const renderForm = (event) => {
-    console.log(event);
-    setFormData(event);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <StorysSelections renderForm={renderForm} />
-    </form>
-  );
+  return <StorysSelections />;
 };
 
 export default StorySelectionForm;
--- ReactJS/Assessment/react-1/madlibs/src/storysSelections.js
+++ ReactJS/Assessment/react-1/madlibs/src/storysSelections.js
@@ -1,25 +1,70 @@
 import React, { useState } from "react";
 import madlibsDB from "./data/madlibsDB";
+import "./storysSelections.css";
 
-const StorysSelections = ({ renderForm }) => {
-  const handleChange = (event) => {
-    renderForm(event.target.value);
+const StorysSelections = () => {
+  const [words, setWords] = useState([]);
+  const [formData, setFormData] = useState("");
+
+  console.log(formData);
+
+  const handleChangeSelection = (event) => {
+    if (event.target.value !== "") {
+      setWords(event.target.value.split(","));
+    } else {
+      setWords([]);
+    }
   };
 
+  const handleChangeField = (event) => {
+    const { name, value } = event.target;
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {};
+
   return (
-    <>
+    <form onSubmit={handleSubmit} className="form-group form-story">
       <label htmlFor="story">Select Story: </label>
-      <select onChange={handleChange} name="story" id="story">
-        <option value="Select One"> Select One </option>
+      <select
+        className="form-control"
+        onChange={handleChangeSelection}
+        name="story"
+        id="story"
+      >
+        <option value=""> Select One </option>
         {madlibsDB.map((s) => (
           // Using title as a key they should be unique,
           // If we where using a real DB
-          <option key={s.title} value={{ madlibsDB }}>
+          <option
+            className="form-control form-title"
+            key={s.title}
+            value={s.words}
+          >
             {s.title}
           </option>
         ))}
       </select>
-    </>
+      {words.map((w) => (
+        <div key={w}>
+          <label className="form-control" htmlFor={w}>
+            {w}:{" "}
+          </label>
+          <input
+            onChange={handleChangeField}
+            className="form-control"
+            id={w}
+            type="text"
+            name={w}
+            placeholder={w}
+            value={formData.word}
+          ></input>
+        </div>
+      ))}
+    </form>
   );
 };
 
