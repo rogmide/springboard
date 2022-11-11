a16d21b2db01c1264ab737e8726bb709a9e15f8c
Roger Delgado <rogmide@gmail.com>
2022-11-11 12:17:23 -0500

    setting up data for app

---
+++ ReactJS/Assessment/react-1/madlibs/src/data/madlibsDB.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    title: "Old Macdonald",
+    word: ["Adjective", "Noun", "Noise", "Animal"],
+    text: `(ADJECTIVE) Macdonald had a (NOUN), E-I-E-I-O
+    and on that (NOUN) he had an (ANIMAL), E-I-E-I-O
+    with a (NOISE) (NOISE) here
+    and a (NOISE) (NOISE) there,
+    here a (NOISE), there a (Noise),
+    everywhere a (NOISE) (NOISE),
+    Adjective Macdonald had a (NOUN), E-I-E-I-O.`,
+  },
+];
