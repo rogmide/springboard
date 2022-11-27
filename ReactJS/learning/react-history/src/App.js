ba7845177d48de756bcd0887af563439aa8452d2
Roger Delgado <rogmide@gmail.com>
2022-11-27 09:56:47 -0500

    more learning

--- ReactJS/learning/react-history/src/App.js
+++ ReactJS/learning/react-history/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Counter number={10} color="blue" />
+      <Counter number={10} />
     </div>
   );
 }
--- ReactJS/learning/react-history/src/Counter.js
+++ ReactJS/learning/react-history/src/Counter.js
@@ -1,15 +1,44 @@
 import React from "react";
 
 class Counter extends React.Component {
+  static defaultProps = { color: "green" };
   constructor(props) {
     super(props);
+    this.state = { count: 0, isHiding: false };
+    // This is ANNOYING
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  componentDidMount() {
+    console.log("Mounted");
+  }
+
+  componentDidUpdate() {
+    console.log("Updated");
+  }
+
+  componentWillUnmount() {
+    console.log("Bye Bye");
   }
 
   render() {
+    console.log("Rendering");
     const { color } = this.props;
     return (
       <div>
         <h1 style={{ color }}>I AM COUNTER</h1>
+        <h3>Count is: {this.state.count}</h3>
+        <button onClick={this.increment}>Add 1</button>
+        <button onClick={this.decrement}>Take 1</button>
       </div>
     );
   }
