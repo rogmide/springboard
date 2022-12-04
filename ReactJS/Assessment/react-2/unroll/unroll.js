4d387411eb023b7f9c66a5a92decb87ff47dacaa
Roger Delgado <rogmide@gmail.com>
2022-12-04 12:48:09 -0500

    unroll Exercise done

--- ReactJS/Assessment/react-2/unroll/unroll.js
+++ ReactJS/Assessment/react-2/unroll/unroll.js
@@ -1,5 +1,48 @@
-function unroll(squareArray) {
+// const square = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16],
+// ];
 
+// const smallerSquare = [
+//   ["a", "b", "c"],
+//   ["d", "e", "f"],
+//   ["g", "h", "i"],
+// ];
+
+let tempResult = [];
+
+function spiralArrays(squareArray) {
+  if (squareArray.length === 0) {
+    return;
+  }
+
+  let fristA = squareArray[0];
+  squareArray.shift();
+  tempResult.push(fristA);
+  let lastA = squareArray.pop();
+  squareArray.map((lastItem) =>
+    tempResult.push(lastItem.splice(lastItem.length - 1))
+  );
+  if (lastA) {
+    tempResult.push(lastA.reverse());
+  }
+  squareArray
+    .reverse()
+    .map((fristItem) => tempResult.push(fristItem.splice(0, 1)));
+
+  spiralArrays(squareArray.reverse());
 }
+
+const unroll = (arr) => {
+  spiralArrays(arr);
+  const result = tempResult.flat();
+  tempResult = [];
+  return result;
+};
+
+// console.log(unroll(square));
+// console.log(unroll(square));
 
 module.exports = unroll;
--- ReactJS/Assessment/react-2/unroll/unroll.test.js
+++ ReactJS/Assessment/react-2/unroll/unroll.test.js
@@ -1,9 +1,28 @@
 const unroll = require("./unroll");
 
 describe("#unroll", function () {
-
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  test("Test Case#1", () => {
+    expect(
+      unroll([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+      ])
+    ).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
+  });
+
+  test("Test Case#2", () => {
+    expect(
+      unroll([
+        ["a", "b", "c"],
+        ["d", "e", "f"],
+        ["g", "h", "i"],
+      ])
+    ).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"]);
+  });
 });
