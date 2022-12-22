ec3181ad4c517dd98b32c0f775420e03d249b06a
Roger Delgado <rogmide@gmail.com>
2022-12-22 11:41:44 -0500

    interesting redux

--- ReactJS/Redux/redux-intro/reduxSetup.js
+++ ReactJS/Redux/redux-intro/reduxSetup.js
@@ -1,1 +1,7 @@
-const store = Redux.createStore();
+const INITIAL_STATE = { count: 0, price: 99 };
+
+const countReducer = (state = INITIAL_STATE, action) => {
+  return state;
+};
+
+const store = Redux.createStore(countReducer);
