2a355156859a47d3ee5d35beacc1f22fcd06cb10
Roger Delgado <rogmide@gmail.com>
2022-12-23 09:32:59 -0500

    more redux learning

--- ReactJS/Redux/redux-react/src/index.js
+++ ReactJS/Redux/redux-react/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+import rootReducer from "./rootReducer";
+import { legacy_createStore as createStore } from "redux";
+
+const store = createStore(rootReducer);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
