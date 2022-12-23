4ee7c0be402d23925b1ea66362b6d41cf7dbde26
Roger Delgado <rogmide@gmail.com>
2022-12-23 15:53:08 -0500

    base app for react-redux

---
+++ ReactJS/Redux/meme-generator-react/src/Reducers/rootReducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = { count: 0, color: "green" };
+
+const rootReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + 1 };
+    case "DECREMENT":
+      return { ...state, count: state.count - 1 };
+    case "RESET":
+      return { ...state, count: 0 };
+    case "MULTIPLY":
+      return { ...state, count: state.count * action.payload };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
