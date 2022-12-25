9c2813b3f5702ac642e7ff2e3d64c5260a207c53
Roger Delgado <rogmide@gmail.com>
2022-12-25 12:47:39 -0500

    interesting

--- ReactJS/Redux/meme-todolist-react-redux/src/Reducers/rootReducer.js
+++ ReactJS/Redux/meme-todolist-react-redux/src/Reducers/rootReducer.js
@@ -1,19 +1,4 @@
-// Some DEFATUL state for the app when load for MEME and TODOS
-
-const INITIAL_STATE = {
-  memes: [
-    {
-      id: 1,
-      top: "Sample Up",
-      buttom: "Sample Down",
-      imgURL:
-        "https://images.unsplash.com/photo-1552664769-aeb2e3f6a67f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-    },
-  ],
-  todos: [{ id: 1, text: "Walk The Doc In Redux", done: false, scrach: false }],
-};
-
-const rootReducer = (state = INITIAL_STATE, action) => {
+const rootReducer = (state = {}, action) => {
   switch (action.type) {
     case "CREATE_MEME":
       return { ...state, memes: [...state.memes, action.payload] };
--- ReactJS/Redux/meme-todolist-react-redux/src/index.js
+++ ReactJS/Redux/meme-todolist-react-redux/src/index.js
@@ -8,8 +8,42 @@ import { Provider } from "react-redux";
 import rootReducer from "./Reducers/rootReducer";
 import { legacy_createStore as createStore } from "redux";
 
-const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// ################################################################
+// To Load and Save Data from Local Store
+// ################################################################
+// I dont fully undestand this need to do more research
+// need to get more familiar with localStore
+// store.subscribe ?
+
+function saveToLocalStorage(state) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
+const store = createStore(rootReducer, loadFromLocalStorage());
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+// ################################################################
+// To Load and Save Data from Local Store
+// ################################################################
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
