004e69da00cf8eaad451ad9c47f3fb7072d25573
Roger Delgado <rogmide@gmail.com>
2022-12-27 14:12:13 -0500

    adding some css

--- ReactJS/Redux/exercise/shoply/src/Products/ProductDetail.js
+++ ReactJS/Redux/exercise/shoply/src/Products/ProductDetail.js
@@ -4,16 +4,15 @@ import "./ProductDetail.css";
 const ProductDetail = ({ name, price, description, image_url }) => {
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img
-        className="card-img-top"
-        style={{  }}
-        src={image_url}
-        alt="Card image cap"
-      />
+      <img className="card-img-top" src={image_url} alt="Card image cap" />
       <div className="card-body">
-        <h5 className="card-title">Name: {name.toUpperCase()}</h5>
-        <h5 className="card-text">Price: {price}</h5>
+        <h5 className="card-title">{name.toUpperCase()}</h5>
+        <h5 className="card-text">Price: {price} USD</h5>
         <p className="card-text">{description}</p>
+      </div>
+      <div>
+        <button className="btn btn-success">Add</button>
+        <button className="btn btn-success">Remove</button>
       </div>
     </div>
   );
--- ReactJS/Redux/exercise/shoply/src/Products/ProductsList.js
+++ ReactJS/Redux/exercise/shoply/src/Products/ProductsList.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import ProductDetail from "./ProductDetail";
+import "./ProductList.css";
 
 const ProductList = () => {
   const items = useSelector((st) => st.products);
   return (
     <>
-      {Object.keys(items).map((id) => (
-        <ProductDetail
-          key={id}
-          name={items[id].name}
-          price={items[id].price}
-          description={items[id].description}
-          image_url={items[id].image_url}
-        />
-      ))}
+      <div className="prodHolder">
+        {Object.keys(items).map((id) => (
+          <ProductDetail
+            key={id}
+            name={items[id].name}
+            price={items[id].price}
+            description={items[id].description}
+            image_url={items[id].image_url}
+          />
+        ))}
+      </div>
     </>
   );
 };
