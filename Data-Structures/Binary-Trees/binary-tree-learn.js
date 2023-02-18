6cbfaced82381501cce664612f19c2dde4a68011
Roger Delgado <rogmide@gmail.com>
2023-02-18 16:11:28 -0500

    exercise for binary trees

--- Data-Structures/Binary-Trees/binary-tree-learn.js
+++ Data-Structures/Binary-Trees/binary-tree-learn.js
@@ -19,6 +19,21 @@ class BinarySearchTree {
   constructor(root = null) {
     this.root = root;
   }
+  traverse(currNode = this.root) {
+    if (currNode.left) this.traverse(currNode.left);
+    console.log(currNode.val);
+    if (currNode.right) this.traverse(currNode.right);
+  }
+  traversePreOrder(currNode = this.root) {
+    console.log(currNode.val);
+    if (currNode.left) this.traverse(currNode.left);
+    if (currNode.right) this.traverse(currNode.right);
+  }
+  traversePostOrder(currNode = this.root) {
+    if (currNode.left) this.traverse(currNode.left);
+    if (currNode.right) this.traverse(currNode.right);
+    console.log(currNode.val);
+  }
 }
 
 const E = new Node("E");
@@ -36,7 +51,10 @@ B.right = D;
 G.left = F;
 
 const tree = new BinarySearchTree(E);
-console.log(E.find("D"));
-console.log(E.find("B"));
-console.log(E.find("A"));
-console.log(E.find("P"));
+// console.log(E.find("D"));
+// console.log(E.find("B"));
+// console.log(E.find("A"));
+// console.log(E.find("P"));
+console.log(tree.traverse());
+console.log(tree.traversePreOrder());
+console.log(tree.traversePostOrder());
